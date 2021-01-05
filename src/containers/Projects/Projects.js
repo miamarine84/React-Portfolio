efea5887a415d98a52048e807e134b8053186913
marco p <miamarine84@yahoo.com>
2021-01-04 21:40:17 -0500

    got ul showing and hiding through react useState on project.js

--- src/containers/Projects/Projects.js
+++ src/containers/Projects/Projects.js
@@ -1,58 +1,68 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../Projects/style.scss'
 import Nav from '../../components/NavBar/Navbar';
 import ProjectsPortion from '../../components/ProjectsPortion/ProjectsPortion'
+
 function Projects() {
-    const [show, setShow] = useState('none');
-    const [buttonHide, setButtonHide] = useState('block')
-    let test=()=>{
-        setShow('inline');
+    const [show, setShow] = useState('inline');
+    const [buttonHide, setButtonHide] = useState('none')
+    let showUl = () => {
+        console.log('here')
+        setShow('none')
+        setButtonHide('inline')
+    }
+
+    let hideUl= () => {
+        setShow('inline')
         setButtonHide('none')
     }
-  return (
- 
-    <div>
-      <Nav/>
-      <ProjectsPortion/>
-      <button class="pulse" onClick={test} style={{display: `${buttonHide}`}}>Pulse</button>
-    
 
+    return (
+        // style={{ display: `${buttonHide}` }}
+        <div>
+            <Nav />
+            <ProjectsPortion />
+            <button onClick={showUl} style={{ display: `${show}` }}>First
+            </button>
 
-      <div id='side-list' style={{display: `${show}`}}>
-                <ul>
-                    <li id='test1'>
+            <div id='side-list' >
+                <button onClick={hideUl} style={{ display: `${buttonHide}` }}>
+                    Pulse
+            </button>
+                <ul  style={{ display: `${buttonHide}` }}>
+                    <li>
                         <a href='https://docs.google.com/document/d/1EmTwYSCCWO7OgSBuDVptaxlR8p0B4trT-A9chlmsZyI/edit' target='_blank'>
                             <img src='https://image.shutterstock.com/image-vector/resume-icon-260nw-477290071.jpg' />
-              
-                        <span>Resume</span>
+
+                            <span>Resume</span>
                         </a>
                     </li>
                     <li>
                         <a href='https://github.com/miamarine84' target='_blank'>
                             <img src='https://alanferrandiz.files.wordpress.com/2020/09/github_logo.png' />
-                      
-                        <span>Github</span>
+
+                            <span>Github</span>
                         </a>
                     </li>
                     <li>
                         <a href='https://www.linkedin.com/in/marcos-p-898b701ab/' target='_blank'>
                             <img src='https://image.shutterstock.com/image-vector/logo-icon-vector-template-260nw-1781944223.jpg' />
-                      
-                        <span>LinkedIn</span>
+
+                            <span>LinkedIn</span>
                         </a>
                     </li>
                     <li>
                         <a href="mailto:marcos.patrocinio1184@gmail.com" target='_blank'>
                             <img src='https://image.shutterstock.com/image-vector/envelope-icon-vector-260nw-768672523.jpg' />
-                     
-                        <span>Email me</span>
+
+                            <span>Email me</span>
                         </a>
                     </li>
                 </ul>
             </div>
-    </div>
-  );
+        </div>
+    );
 }
 
 export default Projects;
